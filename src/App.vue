93f71741d75724a86ee572a628703d668e322626
Cupid Valentine <westerngod@126.com>
2022-11-21 21:30:04 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<script lang="ts" setup>
+import { Menu, MenuItem, Dropdown, Button } from 'ant-design-vue'
+import { ref, onMounted } from 'vue'
+import Driver from 'driver.js'
+import 'driver.js/dist/driver.min.css'
+
+const current = ref<string[]>(['mail'])
+
+const visible = ref(false)
+
+onMounted(async () => {
+  visible.value = true
+  setTimeout(() => {
+    visible.value = false
+    tour()
+  }, 1)
+})
+
+function tour() {
+  const driver = new Driver()
+  driver.defineSteps([
+    {
+      element: '#menu',
+      popover: {
+        className: 'first-step-popover-class',
+        title: 'Title on Popover',
+        description: 'Body of the popover',
+        position: 'bottom'
+      },
+      onNext: () => {
+        visible.value = true
+        // Prevent moving to the next step
+        driver.preventMove()
+
+        // Perform some action or create the element to move to
+        // And then move to that element
+        setTimeout(() => {
+          driver.moveNext()
+        }, 600)
+      }
+    },
+    {
+      element: '#submenu',
+      popover: {
+        title: 'Title on Popover',
+        description: 'Body of the popover',
+        position: 'bottom'
+      },
+      onNext: () => {
+        visible.value = false
+      }
+    }
+  ])
+  driver.start()
+}
+</script>
+
+<template>
+  <div class="box">
+    <Menu v-model:selectedKeys="current" mode="horizontal">
+      <MenuItem key="mail" id="menu">
+        <Dropdown :trigger="['click']" v-model:visible="visible">
+          <a class="ant-dropdown-link" id="alink" @click.prevent>Menu1</a>
+          <template #overlay>
+            <div class="card">
+              <Menu>
+                <MenuItem>
+                  <a href="javascript:;">1st menu item</a>
+                </MenuItem>
+                <MenuItem id="submenu">
+                  <a href="javascript:;">2nd menu item</a>
+                </MenuItem>
+                <MenuItem>
+                  <a href="javascript:;">3rd menu item</a>
+                </MenuItem>
+              </Menu>
+              <Menu>
+                <MenuItem>
+                  <a href="javascript:;">1st menu item</a>
+                </MenuItem>
+                <MenuItem>
+                  <a href="javascript:;">2nd menu item</a>
+                </MenuItem>
+                <MenuItem>
+                  <a href="javascript:;">3rd menu item</a>
+                </MenuItem>
+              </Menu>
+            </div>
+          </template>
+        </Dropdown>
+      </MenuItem>
+      <MenuItem key="app" disabled>Navigation Two</MenuItem>
+    </Menu>
+  </div>
+</template>
+
+<style>
+.box {
+  width: 800px;
+  margin: 20px auto;
+}
+
+.ant-dropdown {
+  width: 100%;
+  top: 66px !important;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
+
+
+createApp(App).mount('#app')
